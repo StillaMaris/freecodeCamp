f569d6f726df7ff187ea1e5959758f67663333bb
StillaMaris <38422879+StillaMaris@users.noreply.github.com>
2023-10-07 09:29:26 +0200

    Problem

---
+++ Assignment/Time Calculator/time_calculator.py
@@ -0,0 +1,17 @@
+def add_time(start, duration, starting_day_of_week=None):
+  '''
+  function that takes in two required parameters and one optional parameter:
+  -a start time in the 12-hour clock format (ending in AM or PM)
+  -a duration time that indicates the number of hours and minutes (hr:min)
+  -(optional) a starting day of the week, case insensitive
+  '''
+  #trasformare lo start in 24hr format
+  
+  
+  #settare il parametro opzionale (se c'è) in lower case
+
+
+  #ritrasformare in 12hr format
+  new_time = 0
+
+  return new_time
