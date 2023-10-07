87d89fb2c8c1b9952afcd65b7992f8e0aae7124e
StillaMaris <38422879+StillaMaris@users.noreply.github.com>
2023-10-07 12:03:42 +0200

    Solved

--- Assignment/Time Calculator/time_calculator.py
+++ Assignment/Time Calculator/time_calculator.py
@@ -6,6 +6,12 @@ def add_time(start, duration, starting_day_of_week=None):
   -(optional) a starting day of the week, case insensitive
   '''
   new_time = ""  #string that should be "hr:min AM|PM"
+  week_days = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+  #settare il parametro opzionale (se c'è) in modo che la prima lettera è maiuscola e le altre minuscole
+  if starting_day_of_week != None:
+    starting_day_of_week = starting_day_of_week.title()
+    
   
   #trasformare lo start in 24hr format
   ampm = start.split(' ')[1]  #["hr:min", "AM"]
@@ -31,35 +37,50 @@ def add_time(start, duration, starting_day_of_week=None):
   if new_time_hr >24:
     days = new_time_hr // 24
     new_time_hr = new_time_hr - days*24
+    
+  if new_time_hr > 12:
+    new_time_ampm = "PM"  
+  elif new_time_hr == 12 and new_time_min > 0:
+    new_time_ampm = "PM"
+  else:
+    new_time_ampm = "AM"
+    
+  if new_time_ampm == "AM" and new_time_hr == 0:
+    new_time_hr = 12
   
   if new_time_hr > 12:
     new_time_hr -= 12
-  
-  
-  if new_time_hr > 12:
-    new_time_ampm = "PM"  
-  else:
-    new_time_ampm = "AM"
   
   if new_time_min < 10:     #aggiungere una stringa '0' se i min sono meno di 10
     new_time_min = '0'+str(new_time_min)
   else:
     new_time_min = str(new_time_min)
     
-  new_time = str(new_time_hr)+':'+new_time_min+' '+new_time_ampm
   
+  new_time = str(new_time_hr)+':'+new_time_min+' '+new_time_ampm
+  if starting_day_of_week != None:
+    if days == 0:
+      new_time += f', {starting_day_of_week}'
+    else:
+      i = week_days.index(starting_day_of_week) +days
+      new_time +=f", {week_days[i%7]}"
+      
   if days == 1:
     new_time += " (next day)"
   elif days > 1:
     new_time += f" ({days} days later)"
+
+  
+      
+  
    
   
+    
   
     
-  #settare il parametro opzionale (se c'è) in lower case
-
-
-  #ritrasformare in 12hr format
+    
+  
+  
   
   
   
